b1046ff0e15ceb97f061d68efaa5a62d1037e58a
NicoBaires <nicolamattina80@gmail.com>
2026-09-02 16:50:32 -0300

    Fix nav links desde otras páginas + comentar botón Hero

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -18,6 +18,8 @@
     const el = document.getElementById(id);
     if (el) {
       el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      window.location.href = href;
     }
     closeMenu();
   }
